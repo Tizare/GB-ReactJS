37b0817759990ba28e7717fbe9ebadb85b8d9731
Tizare <matatova-v@mail.ru>
2022-02-14 12:03:09 +0300

    added  necessary HTML+CSS

--- today/src/App.js
+++ today/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Message from "./Message"
+import React from 'react';
+import { useState } from 'react';
+
+const messageList = [{name:"Bot", message:"Добро пожаловать!"}];
 
 function App(props) {
+  const [list, setList] = useState(messageList)
   return (
     <div className="App">
       <Message today={props.today}/>
-      
+      <div className="ourChat">
+            <form action="#">
+                <input type="text" placeholder="Ваше имя"/>
+                <textarea type="text" placeholder="Ваше сообщение"/>
+                <button>Отправить</button>
+            </form>
+            <div className="allMessage">
+                {list.map((item)=>(
+                  <div className="message">{item.name}:<br/>{item.message}</div>
+                ))}
+            </div>
+        </div>
     </div>
   );
 }
